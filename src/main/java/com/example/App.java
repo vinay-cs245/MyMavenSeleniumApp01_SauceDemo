a1ace976a2d1da87dac31e9c1fc8c624311a05ba
vinay-cs245 <vinaybs8vinay8@gmail.com>
2026-04-16 13:03:19 +0530

    updated app.java to support jenkins

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,20 +3,30 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        WebDriver driver=new ChromeDriver();
+
+public class App {
+
+    public static void main(String[] args) {
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Required for Jenkins / CI environments
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
+
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+
+        driver.quit();
     }
 }
